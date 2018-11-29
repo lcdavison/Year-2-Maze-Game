13fa24b69ba64b875ca3a9b2a38bb49e61b6ac69
lcdavison <luke.davison@talktalk.net>
2018-11-29 13:31:22 +0000

    Added shader loading

--- Source/ResourceManager.cpp
+++ Source/ResourceManager.cpp
@@ -8,7 +8,29 @@ bool ResourceManager::LoadMesh ( const char* path )
 {
 }
 
-bool ResourceManager::LoadShader ( const char* path, char* out_shader )
+bool ResourceManager::LoadShader ( const char* path, std::string& out_shader )
 {
 	std::ifstream file;
+
+	file.open ( path );
+
+	if ( !file.is_open (  ) )
+	{
+		std::cout << "Failed to open file : " << path << std::endl;
+		return false;
+	}
+
+	std::string line;
+	std::stringstream stringbuffer;
+
+	while ( std::getline ( file, line ) )
+	{
+		stringbuffer << line;
+	}
+
+	file.close (  );
+
+	out_shader = stringbuffer.str (  );
+
+	return true;
 }
