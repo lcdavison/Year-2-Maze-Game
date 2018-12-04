c1dcaf3e56bf63e07f3165d44ad158a455caa481
lcdavison <luke.davison@talktalk.net>
2018-12-04 23:35:25 +0000

    Tested writing game logic in external file for commands

---
+++ Source/includes/Scripting/PlayerCommands.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+
+namespace PlayerCommands 
+{
+	static void MoveLeft (  )
+	{
+		std::cout << "Move Left" << std::endl;
+	}
+
+	static void MoveRight (  )
+	{
+		std::cout << "Move Right" << std::endl;
+	}
+};
