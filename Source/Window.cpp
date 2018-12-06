6cad16dd610ccc55b48e76d7a4bc241c5316e9f6
lcdavison <luke.davison@talktalk.net>
2018-12-06 18:25:53 +0000

    Added additional SDL GL attributes

--- Source/Window.cpp
+++ Source/Window.cpp
@@ -29,12 +29,20 @@ bool Window::CreateWindow (  )
 	SDL_GLContext context = SDL_GL_CreateContext ( window );
 
 	SDL_GL_SetAttribute ( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
-
+	
 	SDL_GL_SetAttribute ( SDL_GL_CONTEXT_MAJOR_VERSION, 4 );
-	SDL_GL_SetAttribute ( SDL_GL_CONTEXT_MINOR_VERSION, 6 );
-
+	SDL_GL_SetAttribute ( SDL_GL_CONTEXT_MINOR_VERSION, 5 );
+	
+	SDL_GL_SetAttribute ( SDL_GL_RED_SIZE, 8 );
+	SDL_GL_SetAttribute ( SDL_GL_GREEN_SIZE, 8 );
+	SDL_GL_SetAttribute ( SDL_GL_BLUE_SIZE, 8 );
+	SDL_GL_SetAttribute ( SDL_GL_ALPHA_SIZE, 8 );
+	SDL_GL_SetAttribute ( SDL_GL_DEPTH_SIZE, 16 );
+	
 	SDL_GL_SetAttribute ( SDL_GL_MULTISAMPLEBUFFERS, 1 );
-	SDL_GL_SetAttribute ( SDL_GL_MULTISAMPLESAMPLES, 8 );
+	SDL_GL_SetAttribute ( SDL_GL_MULTISAMPLESAMPLES, 4 );
+
+	SDL_GL_SetAttribute ( SDL_GL_DOUBLEBUFFER, 1 );
  
 	return true;
 }
